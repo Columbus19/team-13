5e7088bb246038c1bca0ce98c6ef349604cb57f9
sharonqiu1 <sharonqiu1@users.noreply.github.com>
2019-10-19 01:58:52 -0400

    Added input fields for basic information.

--- screens/ResumePage.js
+++ screens/ResumePage.js
@@ -12,8 +12,82 @@ class ResumePage extends React.Component {
   render(){
     return(
       <ScrollView>
-        <Text style={{fontSize:24}}>Application Details</Text>
-        <TextInput style={styles.inputContainer}></TextInput>
+        <Text
+            h1
+            style={{ marginBottom: theme.SIZES.BASE / 2 , textAlign:"center"}}
+            color={argonTheme.COLORS.DEFAULT}>
+        Application Details
+        </Text>
+        
+        {/*inputs for basic info*/}
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            First Name
+          </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input right placeholder="ex. John" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Last Name
+          </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input right placeholder="ex. Smith" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Email Address
+          </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input right placeholder="ex. hi@example.com" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Phone Number
+          </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input right placeholder="ex. 888-888-8888" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Current Address
+          </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="Enter current address here" iconContent={<Block />} />
+        </Block>
+        
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Permanent Address
+          </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="Enter permanent address here" iconContent={<Block />} />
+        </Block>
+
+
+
         <TextInput style={styles.inputContainer}></TextInput>
         <TextInput style={styles.inputContainer}></TextInput>
         <TextInput style={styles.inputContainer}></TextInput>
@@ -24,15 +98,17 @@ class ResumePage extends React.Component {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    padding: 100,
-    margin: 50,
-    borderColor: "black",
-    borderWidth: 1,
-    backgroundColor: 'blue'
-  },
-  input: {
-    height: 40,
-    marginBottom: 20,
+    textAlign:"left",
+    color: "black",
+    marginLeft: 15,
+    marginBottom: 15,
+    marginRight: 15,
+    marginTop: 15,
+    borderColor: 'grey',
+    borderWidth: 2,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 18
   }
 });
 
