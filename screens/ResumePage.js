bf2c4e52cab1e5df71a2264ae0316aa9a64bc2aa
sharonqiu1 <sharonqiu1@users.noreply.github.com>
2019-10-19 02:45:52 -0400

    Added input for research/projects.

--- screens/ResumePage.js
+++ screens/ResumePage.js
@@ -19,6 +19,13 @@ class ResumePage extends React.Component {
         Application Details
         </Text>
         
+        <Text
+            h2
+          style={{ marginBottom: theme.SIZES.BASE / 2, textAlign: "center", textDecorationLine: "underline",paddingTop: 20}}
+            color={argonTheme.COLORS.DEFAULT}>
+        General Information
+        </Text>
+
         {/*inputs for basic info*/}
         <Text
             h5
@@ -26,7 +33,7 @@ class ResumePage extends React.Component {
             color={argonTheme.COLORS.DEFAULT}
           >
             First Name
-          </Text>
+        </Text>
         <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
           <Input right placeholder="ex. John" iconContent={<Block />} />
         </Block>
@@ -37,7 +44,7 @@ class ResumePage extends React.Component {
             color={argonTheme.COLORS.DEFAULT}
           >
             Last Name
-          </Text>
+        </Text>
         <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
           <Input right placeholder="ex. Smith" iconContent={<Block />} />
         </Block>
@@ -48,7 +55,7 @@ class ResumePage extends React.Component {
             color={argonTheme.COLORS.DEFAULT}
           >
             Email Address
-          </Text>
+        </Text>
         <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
           <Input right placeholder="ex. hi@example.com" iconContent={<Block />} />
         </Block>
@@ -59,7 +66,7 @@ class ResumePage extends React.Component {
             color={argonTheme.COLORS.DEFAULT}
           >
             Phone Number
-          </Text>
+        </Text>
         <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
           <Input right placeholder="ex. 888-888-8888" iconContent={<Block />} />
         </Block>
@@ -70,7 +77,7 @@ class ResumePage extends React.Component {
             color={argonTheme.COLORS.DEFAULT}
           >
             Current Address
-          </Text>
+        </Text>
         <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
           <Input multiline="true" scroll="true" right placeholder="Enter current address here" iconContent={<Block />} />
         </Block>
@@ -81,16 +88,202 @@ class ResumePage extends React.Component {
             color={argonTheme.COLORS.DEFAULT}
           >
             Permanent Address
-          </Text>
+        </Text>
         <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
           <Input multiline="true" scroll="true" right placeholder="Enter permanent address here" iconContent={<Block />} />
         </Block>
 
+        {/*Start of resumé specifics*/}
+        <Text
+            h2
+            style={{ marginBottom: theme.SIZES.BASE / 2 , textAlign:"center", textDecorationLine:"underline", paddingTop: 20}}
+            color={argonTheme.COLORS.DEFAULT}>
+        Resume Specifics
+        </Text>
 
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Objective Statement
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="Enter Objective Statement" iconContent={<Block />} />
+        </Block>
 
-        <TextInput style={styles.inputContainer}></TextInput>
-        <TextInput style={styles.inputContainer}></TextInput>
-        <TextInput style={styles.inputContainer}></TextInput>
+        <Text
+          h4
+          style={{ marginBottom: theme.SIZES.BASE / 2, textAlign: "left", textDecorationLine: "underline", paddingTop: 10 }}
+          color={argonTheme.COLORS.DEFAULT}>
+          Education
+        </Text>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            School
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Ohio State University" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Location of School
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Columbus, Ohio" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Field of Study (Major)
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Computer Science" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Field of Study (Minor(s), comma separated)
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Mathematics" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Current Overall GPA
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. 3.47" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Expected Graduation Month and Year
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. May 2021" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Relevant Coursework (comma separated)
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Data Structures and Algorithms, Linear Algebra, ..." iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Technical/Non-Technical Skills (comma separated)
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Microsoft Office Suite, Bilingual in English and Hindi, ..." iconContent={<Block />} />
+        </Block>
+        
+        {/*Projects/Research Inputs*/}
+        <Text
+            h2
+            style={{ marginBottom: theme.SIZES.BASE / 2 , textAlign:"center", textDecorationLine:"underline", paddingTop: 20}}
+            color={argonTheme.COLORS.DEFAULT}>
+            Work and Technical Experience
+        </Text>
+
+        <Text
+          h3
+          style={{ marginBottom: theme.SIZES.BASE / 2, textAlign: "center", textDecorationLine: "underline", paddingTop: 10 }}
+          color={argonTheme.COLORS.DEFAULT}>
+          Projects/Research
+        </Text>
+
+        <Text
+          h4
+          style={{ marginBottom: theme.SIZES.BASE / 2, textAlign: "left", fontWeight:"bold"}}
+          color={argonTheme.COLORS.DEFAULT}>
+          Projects/Research(1)
+        </Text>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Project/Research Name
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Sea Perch Robot Project" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Role
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. Team Member" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Start Date
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. October 2013" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            End date (Enter Present if current project/research)
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="ex. November 2014" iconContent={<Block />} />
+        </Block>
+
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 , marginLeft: 15}}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            Description/Duties (Can be multiple sentences)
+        </Text>
+        <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+          <Input multiline="true" scroll="true" right placeholder="Enter description here" iconContent={<Block />} />
+        </Block>
+        
       </ScrollView>
     );
   }
