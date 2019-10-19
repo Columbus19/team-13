cb27762bacfd63d0015b50598902b6fb948bfc62
Chidubem Okoli <okoli@purdue.edu>
2019-10-19 03:14:30 -0400

    Made mock interview record buttons

--- screens/MockInterview.js
+++ screens/MockInterview.js
@@ -14,12 +14,14 @@ class MockInterview extends React.Component {
 
         this.state = {
             test: 0
+            // test1: 0
         };
     }
 
     onHandleChangeTest = () => {
         this.setState({
             test: ++this.state.test
+            // test1: ++this.state.test1
         });
     }
 
@@ -29,12 +31,58 @@ class MockInterview extends React.Component {
         <Text style={{fontSize:24}}>Mock Interview</Text>
         <Text style={{fontSize:15}}> For this mock interview you would be prompted with 6 questions 
         regarding your past group or academic experiences. Before answering the questions you would
-        be given time to think up an answer and when that time is up. Present your answer through a video 
-        recording that would begin recording at your time.
-        
-        </Text>
+        be given time to think up an answer and when that time is up you will present your answer through an audio 
+        recording that would begin recording at your time.</Text>
+        {/* <Text style={{fontSize:16}}>1. Tell me about a time you were in a ... </Text> */}
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 }}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            1. Tell me about a time that you were in ...
+          </Text>
         <Text>{this.state.test}</Text>
-        <Button onPress={this.onHandleChangeTest}>Test</Button>
+        <Button onPress={this.onHandleChangeTest}>Record</Button>
+        <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 }}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            2. What is your reason for ...
+          </Text>
+          <Button onPress={this.onHandleChangeTest}>Record</Button>
+          <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 }}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            3. What his best ....
+          </Text>
+          <Button onPress={this.onHandleChangeTest}>Record</Button>
+          <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 }}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            4. Do you have any previous experience ...
+          </Text>
+          <Button onPress={this.onHandleChangeTest}>Record</Button>
+          <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 }}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            5. When you took this class ....
+          </Text>
+          <Button onPress={this.onHandleChangeTest}>Record</Button>
+          <Text
+            h5
+            style={{ marginBottom: theme.SIZES.BASE / 2 }}
+            color={argonTheme.COLORS.DEFAULT}
+          >
+            6. How would you handle a situation where ...
+          </Text>
+          <Button onPress={this.onHandleChangeTest}>Record</Button>
       </ScrollView>
     );
   }
